81bd38474e8f84f7b3baada28d9ed3b62e3401b8
Inkeet C <inkeetcha234@gmail.com>
2024-12-19 15:17:26 +0530

    modularized path into an variable

--- shell.js
+++ shell.js
@@ -1,6 +1,9 @@
+const currentDirectory = '~';
+
 const shell = function () {
+  console.log('You are inside Loki');
   while (true) {
-    const command = prompt('You are inside Loki');
+    const command = prompt('You are currently in:', currentDirectory);
     console.log('You entered: ', command);
   }
 };
