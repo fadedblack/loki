8021c4572533e610155ae72aff26c9741ec2699a
Inkeet C <inkeetcha234@gmail.com>
2024-12-19 21:19:54 +0530

    passing command map through main function, changed approaches here n there

--- loki.js
+++ loki.js
@@ -1,56 +1,58 @@
 let currentPath = '~';
 
 const echo = function (args) {
-  console.log(args.join(' '));
+  return args.join(' ');
 };
 
 const cd = function (args) {
   currentPath = args.join('');
+  return currentPath;
 };
 
 const pwd = function () {
-  console.log(currentPath);
+  return currentPath;
 };
 
-const mapCommandsAndFunctions = [["pwd", pwd], ["cd", cd], ["echo", echo]];
+const commandMap = [
+  ["pwd", pwd],
+  ["cd", cd],
+  ["echo", echo]
+];
 
-const executeCommand = function (command, args) {
-  return command(args);
-};
 
-const isValidCommand = function (command) {
-  return command !== undefined;
-};
-
-const isCommandExist = function (command) {
-  const isCommandExist = mapCommandsAndFunctions.find(function (inBuiltCommand) {
+const withCommands = function (command, commandMap) {
+  const isCommandExist = commandMap.find(function (inBuiltCommand) {
     return inBuiltCommand[0] === command;
   });
 
   return isCommandExist;
 };
 
-const runCommand = function (commandArguments) {
+const runCommand = function (commandArguments, commandMap) {
   const [command, ...args] = commandArguments.split(' ');
-  const validCommand = isCommandExist(command);
+  const commandExists = withCommands(command, commandMap);
 
-  if (isValidCommand(validCommand)) {
-    executeCommand(validCommand[1], args);
+  if (!commandExists) {
+    return "Command Not Found!!";
   }
 
+  const actualCommand = commandExists[1];
+  return actualCommand(args);
 };
 
-const launchShell = function () {
+const display = function (string) {
+  console.log(string);
+};
+
+const launchShell = function (commandMap) {
   console.log('You are inside Loki');
   while (true) {
     const command = prompt('Loki `' + currentPath + '`:');
 
-    runCommand(command);
+    display(runCommand(command, commandMap));
   }
 };
 
-launchShell();
+launchShell(commandMap);
 
-// Space is not incorporated in echo function
-// haven't handled in correct commands
 // haven't handled in correct arguments
