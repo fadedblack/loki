61610de9e49e0533ec4de55f222f3e57e0c1dfa3
Inkeet C <inkeetcha234@gmail.com>
2024-12-19 17:10:38 +0530

    added runCommand to run a command

--- loki.js
+++ loki.js
@@ -1,10 +1,18 @@
 const currentDirectory = '~';
 
+const runCommand = function (commandArguments) {
+  const [command, ...args] = commandArguments.split(' ');
+
+  return [command, args];
+};
+
 const shell = function () {
   console.log('You are inside Loki');
   while (true) {
-    const command = prompt('You are currently in:', currentDirectory);
+    const command = prompt('You are currently in ' + currentDirectory + ':');
     console.log('You entered: ', command);
+
+    console.log(runCommand(command));
   }
 };
 
