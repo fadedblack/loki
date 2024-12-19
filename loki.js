3189e63c48aa94da9a912db6750a3b28c7de4fbf
Inkeet C <inkeetcha234@gmail.com>
2024-12-19 17:16:17 +0530

    added validCommand to check validity of command

--- loki.js
+++ loki.js
@@ -1,7 +1,21 @@
 const currentDirectory = '~';
 
+const validCommand = function (command) {
+  const commandList = ["ls", "echo"];
+
+  const isValidCommand = commandList.find(function (inBuiltCommand) {
+    return inBuiltCommand === command;
+  });
+
+  return isValidCommand !== undefined;
+};
+
 const runCommand = function (commandArguments) {
   const [command, ...args] = commandArguments.split(' ');
+
+  if (validCommand(command)) {
+    execute(command);
+  }
 
   return [command, args];
 };
